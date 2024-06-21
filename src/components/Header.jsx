2485ed66ebe27602dc1fe4321fbf1144402b6047
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-06-20 21:55:59 -0500

    Moved print button to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
 import { GiRaven } from "react-icons/gi";
+import { IoPrint } from "react-icons/io5";
 import "../styles/Header.css";
 
 function Header() {
   return (
     <header className="header">
-      <GiRaven />
-      Odin CV Creator
+      <div className="logo">
+        <GiRaven />
+        Odin CV Creator
+      </div>
+      <button
+        className="printBtn"
+        onClick={() => {
+          window.print();
+        }}>
+        <IoPrint /> Print
+      </button>
     </header>
   );
 }
--- src/components/LivePreview.jsx
+++ src/components/LivePreview.jsx
@@ -1,7 +1,6 @@
 import Personal from "./Personal";
 import Experience from "./Experience";
 import Education from "./Education";
-import { IoPrint } from "react-icons/io5";
 import "../styles/LivePreview.css";
 
 function LivePreview({ datas }) {
@@ -9,18 +8,9 @@ function LivePreview({ datas }) {
 
   return (
     <div className="livePreview">
-      <div className="preview">
-        <Personal data={personalData} />
-        <Experience data={experienceData} />
-        <Education data={educationData} />
-      </div>
-      <button
-        className="printBtn"
-        onClick={() => {
-          window.print();
-        }}>
-        <IoPrint /> Print
-      </button>
+      <Personal data={personalData} />
+      <Experience data={experienceData} />
+      <Education data={educationData} />
     </div>
   );
 }
